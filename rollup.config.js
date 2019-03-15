6e9eaae8052ce899250cc9d765f1e95b7d75f7b0
Alex <alexnezhynsky@gmail.com>
2019-03-14 22:55:46 -0400

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
-import string from 'rollup-plugin-string'
+import { string } from 'rollup-plugin-string'
 
 const dist = 'dist'
 const bundle = 'bundle'
